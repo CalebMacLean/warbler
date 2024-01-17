b68df0a0be3cf9d5041c2be0d7770a64be4a17de
CalebMacLean <CMaclean98@gmail.com>
2024-01-17 15:34:00 -0800

    added logic to seed.py that connected it to deployed database before populating it

--- seed.py
+++ seed.py
@@ -1,8 +1,23 @@
 """Seed database with sample data from CSV Files."""
 
 from csv import DictReader
-from app import db
-from models import User, Message, Follows
+from models import db, connect_db, User, Message, Follows
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    os.environ.get('DATABASE_URL', 'postgresql:///warbler'))
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+
+app.app_context().push()
+
+connect_db(app)
 
 
 db.drop_all()
